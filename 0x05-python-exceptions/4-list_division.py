74537ed95be27dd87aa1512498423196c0250a2d
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-12-19 16:43:16 +0000

    task 4 cor

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-  """Divides two lists element by element.
+    """Divides two lists element by element.
     Args:
         my_list_1 (list): The first list.
         my_list_2 (list): The second list.
