504f45493251696bf50079452a56df6ad34d3d46
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-12-20 13:59:26 +0000

    task0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+#Define a class Square.
 
 class Square:
-    """Empty class that defines a square."""
+    #Empty class that defines a square.
     pass
