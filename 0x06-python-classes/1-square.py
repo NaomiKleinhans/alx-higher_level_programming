448f8b0507ff207052e3749dcb6d42a74cacd0ed
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-12-20 14:08:15 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
 class Square:
     """Defines a square"""
-    def __init__(self,size):
-        """Initialises the data"""
-        self.__size = size
+    def __init__(self, size):
+        """Initialises square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size #: size of the square
