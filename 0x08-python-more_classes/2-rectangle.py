d0675c3f454871b91145b8763067d7654b2038fc
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-01-04 04:55:36 -0800

    task 2 correction

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,7 +7,6 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -46,7 +45,7 @@ class Rectangle:
         return (self.__width * self.__height)
 
     def perimeter(self):
-       """Return the perimeter of the Rectangle."""
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
