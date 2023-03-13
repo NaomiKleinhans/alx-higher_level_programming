d78a41c56f360aebad09cf6e0abd5c1e2f1d6b0c
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-03-13 04:59:44 -0700

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const args = process.argv.length;
-
-console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
