99967e7cab8b03bcf70759d7d86603fcb1ac1e3c
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-03-14 13:27:22 -0700

    correction

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
+  constructor(h, w) {
+    this.height = h;
+    this.width = w;
   }
 };
