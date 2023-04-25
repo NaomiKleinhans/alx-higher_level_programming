d032e1819f8fb5307f68a01a54be4e77b4f13b90
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-04-25 11:41:53 +0000

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', (error) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(`Successfully wrote to ${filePath}`);
+});
