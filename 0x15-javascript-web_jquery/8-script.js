ce0aec62f499082e2d0ee8fdf37837b18d9cd51d
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-07 23:21:40 +0200

    tasks completed jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+$.getJSON(
+	'https://swapi-api.alx-tools.com/api/films/?format=json',
+	function (data) {
+		data.results.forEach(function (movie) {
+			$('#list_movies').append('<li>' + movie.title + '</li>')
+		})
+	}
+)
